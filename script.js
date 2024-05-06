95796ab44a5632a309fce506ac85da0e5a81cfdb
Taras Pustovit <pustovittaras48@gmail.com>
2024-05-06 13:13:06 +0200

    Nytt header

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const navbar = document.getElementById("navbar");
 const navbarToggle = document.getElementById("navbar-toggle");
+const books = [
+  { title: "Pride and Prejudice", author: "Jane Austen" },
+  { title: "To Kill a Mockingbird", author: "Harper Lee" },
+  { title: "1984", author: "George Orwell" },
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+];
 
 window.addEventListener("resize", function () {
   if (window.innerWidth <= 760) {
@@ -18,3 +24,15 @@ navbarToggle.addEventListener("click", function () {
     navbar.style.display = "none";
   }
 });
+
+function displayBooks() {
+  const bookList = document.getElementById("book-list");
+  bookList.innerHTML = "";
+  books.forEach((book) => {
+    const bookItem = document.createElement("div");
+    bookItem.classList.add("book");
+    bookItem.innerHTML = `<h3>${book.title}</h3><p>Av ${book.author}</p>`;
+    bookList.appendChild(bookItem);
+  });
+}
+window.onload = displayBooks;
