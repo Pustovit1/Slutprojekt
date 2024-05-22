17cd5ac67495bcd287874dfc7d9bbfa3a160d26c
Taras Pustovit <pustovittaras48@gmail.com>
2024-05-22 10:35:18 +0200

    Medlemförmulär och tabell, css ändringar för knappar, js kod fix

--- script.js
+++ script.js
@@ -26,29 +26,65 @@ const books = [
     image: "Lesbiska_ligan.jpg",
     Link: "https://www.goodreads.com/book/show/57977988-lesbiska-ligan-en-sann-kriminalhistoria?ref=nav_sb_ss_1_8",
   },
+  {
+    title: "Lesbiska Ligan",
+    author: "Mian Lodalen",
+    image: "Lesbiska_ligan.jpg",
+    Link: "https://www.goodreads.com/book/show/57977988-lesbiska-ligan-en-sann-kriminalhistoria?ref=nav_sb_ss_1_8",
+  },
+  {
+    title: "Lesbiska Ligan",
+    author: "Mian Lodalen",
+    image: "Lesbiska_ligan.jpg",
+    Link: "https://www.goodreads.com/book/show/57977988-lesbiska-ligan-en-sann-kriminalhistoria?ref=nav_sb_ss_1_8",
+  },
+  {
+    title: "Lesbiska Ligan",
+    author: "Mian Lodalen",
+    image: "Lesbiska_ligan.jpg",
+    Link: "https://www.goodreads.com/book/show/57977988-lesbiska-ligan-en-sann-kriminalhistoria?ref=nav_sb_ss_1_8",
+  },
+  {
+    title: "Lesbiska Ligan",
+    author: "Mian Lodalen",
+    image: "Lesbiska_ligan.jpg",
+    Link: "https://www.goodreads.com/book/show/57977988-lesbiska-ligan-en-sann-kriminalhistoria?ref=nav_sb_ss_1_8",
+  },
+  {
+    title: "Lesbiska Ligan",
+    author: "Mian Lodalen",
+    image: "Lesbiska_ligan.jpg",
+    Link: "https://www.goodreads.com/book/show/57977988-lesbiska-ligan-en-sann-kriminalhistoria?ref=nav_sb_ss_1_8",
+  },
 ];
 
-window.addEventListener("resize", function () {
-  if (window.innerWidth <= 760) {
+window.addEventListener("load", function () {
+  if (window.innerWidth <= 800) {
     navbar.style.display = "none";
-    navbarToggle.style.display = "block";
+    navbarToggle.style.display = "flex";
   } else {
-    navbar.style.display = "block";
+    navbar.style.display = "flex";
     navbarToggle.style.display = "none";
+    navbar.style.right = "0px";
   }
 });
 
 navbarToggle.addEventListener("click", function () {
   if (navbar.style.display === "none") {
-    navbar.style.display = "block";
+    navbar.style.display = "flex";
+    navbar.style.right = "120px";
+    navbar.style.flexDirection = "column";
   } else {
     navbar.style.display = "none";
+    navbar.style.flexDirection = "row";
   }
 });
 
 function displayBooks() {
   const bookContainer = document.getElementById("book-container");
   const scrollButtons = document.getElementById("scroll-buttons");
+
+  if (!bookContainer) return;
 
   bookContainer.innerHTML = "";
   books.forEach((book) => {
@@ -70,7 +106,6 @@ function displayBooks() {
     bookContainer.appendChild(bookItem);
   });
 
-  // Kontrollera om böckerna är längre än fönstret för att visa/hide scrollpilarna
   if (bookContainer.scrollWidth > window.innerWidth) {
     bookContainer.classList.add("scrollable");
     scrollButtons.classList.add("scrollable");
@@ -80,21 +115,53 @@ function displayBooks() {
   }
 }
 
-// Visa böcker när sidan laddas
 window.onload = displayBooks;
 
-// Lyssna på fönstrets storleksändringar för att uppdatera scrollpilarna
 window.addEventListener("resize", displayBooks);
 
-// Lyssna på klick på scrollpilar för att bläddra genom böcker
-document.getElementById("scroll-left").addEventListener("click", function () {
-  document
-    .getElementById("book-container")
-    .scrollBy({ left: -200, behavior: "smooth" });
-});
+let scrollLeft = document.getElementById("scroll-left");
+if (scrollLeft) {
+  scrollLeft.addEventListener("click", function () {
+    document
+      .getElementById("book-container")
+      .scrollBy({ left: -200, behavior: "smooth" });
+  });
+}
+let scrollRight = document.getElementById("scroll-right");
+if (scrollRight) {
+  scrollRight.addEventListener("click", function () {
+    document
+      .getElementById("book-container")
+      .scrollBy({ left: 200, behavior: "smooth" });
+  });
+}
 
-document.getElementById("scroll-right").addEventListener("click", function () {
-  document
-    .getElementById("book-container")
-    .scrollBy({ left: 200, behavior: "smooth" });
-});
+document
+  .getElementById("userForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const className = document.getElementById("class").value;
+    const email = document.getElementById("email").value;
+
+    console.log(firstName, lastName, className, email);
+
+    const table = document
+      .getElementById("userTable")
+      .getElementsByTagName("tbody")[0];
+    const newRow = table.insertRow();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+    const cell4 = newRow.insertCell(3);
+    const cell5 = newRow.insertCell(4);
+
+    cell1.textContent = firstName;
+    cell2.textContent = lastName;
+    cell3.textContent = className;
+    cell4.textContent = email;
+    cell5.textContent = "Medlem";
+    document.getElementById("userForm").reset();
+  });
